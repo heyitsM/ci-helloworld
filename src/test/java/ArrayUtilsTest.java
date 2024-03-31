cdfe502a85235cded32ac643cc8563cbc9a6668f
Emily Berger <bergeremily2@gmail.com>
2024-03-30 21:25:19 -0400

    yay its working!!!

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -16,33 +16,33 @@ public class ArrayUtilsTest {
         // if x is empty, there is no error.
         assertEquals(-1, ArrayUtils.findLast(new int[]{}, 3));
     }
-//
-//    @Test
-//    public void testFindLastNonExistent() {
-//        assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
-//    }
-//
-//    @Test
-//    public void testFindLastExistsFirstElement() {
-//        assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
-//    }
-//
-//    @Test
-//    public void testOddOrPosNull() {
-//        // Do not execute fault
-//        assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
-//    }
-//
-//
-//    @Test
-//    public void testOddOrPosAllPositives() {
-//        // Any nonempty x with only non-negative elements works, because the first part of the
-//        // compound if-test is not necessary unless the value is negative.
-//        assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
-//    }
-//
-//    @Test
-//    public void testOddOrPositiveBothPositivesAndNegatives() {
-//        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
-//    }
+
+    @Test
+    public void testFindLastNonExistent() {
+        assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
+    }
+
+    @Test
+    public void testFindLastExistsFirstElement() {
+        assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
+    }
+
+    @Test
+    public void testOddOrPosNull() {
+        // Do not execute fault
+        assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
+    }
+
+
+    @Test
+    public void testOddOrPosAllPositives() {
+        // Any nonempty x with only non-negative elements works, because the first part of the
+        // compound if-test is not necessary unless the value is negative.
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testOddOrPositiveBothPositivesAndNegatives() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
+    }
 }
