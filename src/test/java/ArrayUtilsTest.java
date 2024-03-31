b8806b3a5b1710118b21348b094c6814a3245fb2
Emily Berger <bergeremily2@gmail.com>
2024-03-30 21:57:30 -0400

    added tests for coverage for countOf

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,19 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void testCountOfArrayWithNoElements() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{}, 0));
+    }
+
+    @Test
+    public void testCountOfArrayWithNoTargetElements() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{1, 2, 3}, 0));
+    }
+
+    @Test
+    public void testCountOfArrayWithTargetElements() {
+        assertEquals(2, ArrayUtils.countOf(new int[]{1, 2, 3, 2}, 2));
+    }
 }
